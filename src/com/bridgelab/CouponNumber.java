5462529d1630d13d2f1ac6e96fe979918d99acc8
sumitmanapure <sumitmanapure1234@gmail.com>
2023-04-07 10:57:59 +0530

    first commit

--- src/com/bridgelab/CouponNumber.java
+++ src/com/bridgelab/CouponNumber.java
@@ -2,56 +2,29 @@ package com.bridgelab;
 
 public class CouponNumber {
     public static void main(String[] args) {
-
         /*
-
-         * char array to get characters for coupen
-
+        * char array to get characters for
          */
-
         char[] chars = "abcdefghijklmnop".toCharArray();
-
         /*
-
-         * length of coupon
-
-         */
-
+* length of coupon
+ */
         int max = 100000000;
 
         /*
-
-         * random value
-
-         */
-
+* random value
+*/
         double d = Math.random();
-
         int random = (int) (d * max);
-
         String sb = "";
-
         System.out.println("random double " + d + " random at start " + random);
-
-
-
         while (random > 0) {
-
             sb = sb + (chars[random % chars.length]);
-
             random /= chars.length;
-
             System.out.println(random + "  " + random % chars.length + " sb is " + sb);
-
         }
-
-
-
         String couponCode = sb.toString();
-
         System.out.println("Coupon Code: " + couponCode);
-
     }
-
 }
 
